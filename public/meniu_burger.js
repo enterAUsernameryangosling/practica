905432ec99cf003794d63174a7631e5d49b4f77a
enterAUsernameryangosling <sandubucatari@gmail.com>
2024-05-27 07:53:03 +0000

    fully responsive home

---
+++ public/meniu_burger.js
@@ -0,0 +1,20 @@
+let checked = 0
+
+function meniu_burger() {
+    let nav = document.getElementById("mobile_nav")
+
+    if (checked === 0) {
+        nav.style = "opacity: 1"
+        checked = 1
+
+        document.getElementById("logo_text").style.color = "#020617"
+        document.getElementById("burger").style.color = "#020617"
+    }
+    else {
+        nav.style = "opacity: 0"
+        checked = 0
+
+        document.getElementById("logo_text").style.color = "white"
+        document.getElementById("burger").style.color = "white"
+    }
+}
